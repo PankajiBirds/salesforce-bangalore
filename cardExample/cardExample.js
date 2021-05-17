cdf0bfeb684d8863c41cd01f1d1ea7bf96583c49
PankajiBirds <pankajgoyal.ibirdscollege@gmail.com>
2021-05-17 17:35:22 +0530

    17-05-2021

---
+++ cardExample/cardExample.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class CardExample extends LightningElement {
+
+    username = "Sumit Kumar Sharma"
+    status = "Active";
+
+    data = {
+        name : 'Anita',
+        contact : '8619251496',
+        email : 'anita.k@gmail.com',
+        status : 'Active'
+    };
+}
